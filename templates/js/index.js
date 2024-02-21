16f55cfcc3f54e63a3da2311664058c93f052c0d
lllzh <1697285056@qq.com>
2024-02-22 02:26:15 +0800

    管理员置顶与删除功能

--- templates/js/index.js
+++ templates/js/index.js
@@ -54,21 +54,52 @@ $index = {
     listItem: function (info) {
         // 显示数据页面
         var html = '';
-        html += '<div class="recruitmentPost">';
-        html += '    <div class="profile">';
+        html += '<div class="comment">';
+        html += '    <div class="peo_info">';
+        html += '        <div class="peo_desc">';
         html += '            <ul>';
-        html += '                <li class="base_comment"><em>' + info.type.join('</em><em>') + '</em><span>' + info.update_date + '</span></li>';
-        html += '                <li class="title_comment">' + (info.top ? '<em class="top">置顶</em>' : '') + '<em class="finishe">' + (info.finishe ? '已' : '未') + '完成</em></li>';
-        html += '                <li class="direction">' + info.direction.join(' • ') + ' ' + info.tag.join(' • ') + '</li>';
+        html += '                <li class="base_comment"><em>' + info.type.join('</em><em>') + '</em><span>' + info.update_date.substring(0, 10) + '</span></li>';
+        html += '                <li class="title_comment">' + (info.top ? '<em class="top">置顶</em>' : '') + '<em class="finishe">' + (info.finishe ? '已' : '未') + '完成</em>' + info.direction.join(' • ') + ' • ' + info.tag.join(' • ') + '</li>';
         html += '            </ul>';
+        html += '        </div>';
         html += '    </div>';
-        html += '    <div class="detailContent">';
-        html += '        <div class="contentArea">';
-        html += '            <p class="postContent">' + info.content + '<a href=""<a href="./detail.html?id=' + info.article_id + '" class="show_all">...</a></p>';
-        html += '            <p class="startTime">开始时间：' + info.start_time + '</p>';
-        html += '            <p class="endTime">结束时间：' + info.end_time + '</p>';
-        html += '            <p class=">联系方式：' + info.contact + '</p>';
+        html += '    <div class="comment_content">';
+        html += '        <div class="content_area">';
+        html += '            <p class="ellipsis3">' + info.content + '<a href="./detail.html?id=' + info.article_id + '" class="show_all">...</a></p>';
+        html += '            <p><em>开始时间：</em>' + info.start_time.substring(0, 10) + '</p>';
+        html += '            <p><em>结束时间：</em>' + info.end_time.substring(0, 10) + '</p>';
+        html += '            <p><em>联系方式：</em>' + info.contact + '</p>';
         html += '            <p><a href="./detail.html?id=' + info.article_id + '" class="show_all">查看详情</a></p>';
+        if (role) {
+            html += '<button class="delete-button" onclick="deletePost(this)"> < img src = "./images/delete.jpeg" alt = "删除" > </button >'
+            html += ' <button button class="sticky-button"><img src="./images/up.jpeg" alt="置顶"</button>'
+        }
+        function deletePost(button) {
+            let post = button.closest('.comment')
+            post.remove();
+        }
+        $('.sticky-button').on('click', function () {
+            // 根据当前的 top 值决定要发送的请求体内容
+            let currentTopValue = info.top;
+            const newTopValue = currentTopValue === 0 ? 1 : 0;
+
+            // 发送改变top值的请求
+            $.ajax({
+                url: 'http://116.62.103.210:8080/      ',
+                type: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify({ top: newTopValue }),
+                success: function () {
+                    // 请求成功后更新当前 top 值
+                    currentTopValue = newTopValue;
+                    // 请求成功后刷新页面
+                    location.reload();
+                },
+                error: function () {
+                    console.error('置顶请求出错');
+                }
+            });
+        });
         html += '        </div>';
         /*html += '        <div class="content_bottom">';
         html += '            <ul>';
